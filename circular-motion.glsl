195e0849cbd50afc898528d524474731523d63fe
0918nobita <nobita.0918@gmail.com>
2019-08-23 21:33:29 +0900

    ♻️ clamp 関数を用いてリファクタリングした

--- circular-motion.glsl
+++ circular-motion.glsl
@@ -5,29 +5,17 @@ precision mediump float;
 uniform float u_time;  // time
 uniform vec2  u_resolution;  // resolution
 
-float oneThird = 1. / 3.;
-float twoThird = 2. / 3.;
-
 void main(void){
   // 画面中央を原点とする座標系での描画位置
   vec2 p = (gl_FragCoord.xy * 2. - u_resolution) / min(u_resolution.x, u_resolution.y);
 
   float periodic = abs(cos(u_time * 0.2));
 
-  float red = 0.;
-  float green = 0.;
-  float blue = 0.;
+  float red = clamp(3. * periodic - 2., 0., 1.);
+  float green = clamp(-3. * periodic + 2., 0., 1.);
+  float blue = clamp(3. * periodic, 0., 1.);
 
-  if (periodic > twoThird) {
-    red = (periodic - twoThird) * 3.;
-    blue = 1.;
-  } else if (periodic > oneThird) {
-    green = 1. - (periodic - oneThird) * 3.;
-    blue = 1.;
-  } else {
-    blue = periodic * 3.;
-    green = 1.;
-  }
+  vec3 baseColor = vec3(red, blue, green) / 25.;
 
   vec3 destColor = vec3(0.);
 
@@ -38,7 +26,7 @@ void main(void){
     // 画面中央を中心として回転する点の座標
     vec2 q = p + vec2(cos(u_time * speed), sin(u_time * speed)) * 0.7;
     // 描画位置の色を，点qと原点の距離に応じて変化
-    destColor += vec3(red, blue, green) / 25. / length(q);
+    destColor += baseColor / length(q);
   }
 
   gl_FragColor = vec4(destColor, 1);
