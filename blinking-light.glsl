42ed2b0ab3d52611d7284d175e123dc882cf6322
0918nobita <nobita.0918@gmail.com>
2019-08-23 18:34:59 +0900

    ✏️ uniform 変数の名称を修正した

--- blinking-light.glsl
+++ blinking-light.glsl
@@ -1,9 +1,9 @@
 precision mediump float;
-uniform float t;  // time
-uniform vec2  r;  // resolution
+uniform float u_time;  // time
+uniform vec2  u_resolution;  // resolution
 
 void main(void) {
-  vec2 p = (gl_FragCoord.xy * 2. - r) / min(r.x, r.y);
-  float l = 0.1 / length(p) * sin(t * 3.5);
+  vec2 p = (gl_FragCoord.xy * 2. - u_resolution) / min(u_resolution.x, u_resolution.y);
+  float l = 0.1 / length(p) * sin(u_time * 3.5);
   gl_FragColor = vec4(l, 0., 0., 1);
 }
--- circular-motion.glsl
+++ circular-motion.glsl
@@ -1,15 +1,15 @@
 precision mediump float;
-uniform float t;       // time
-uniform vec2  r;       // resolution
+uniform float u_time;  // time
+uniform vec2  u_resolution;  // resolution
 
 float oneThird = 1. / 3.;
 float twoThird = 2. / 3.;
 
 void main(void){
   // 画面中央を原点とする座標系での描画位置
-  vec2 p = (gl_FragCoord.xy * 2. - r) / min(r.x, r.y);
+  vec2 p = (gl_FragCoord.xy * 2. - u_resolution) / min(u_resolution.x, u_resolution.y);
 
-  float periodic = abs(cos(t * 0.2));
+  float periodic = abs(cos(u_time * 0.2));
 
   float red = 0.;
   float green = 0.;
@@ -33,7 +33,7 @@ void main(void){
     // 個々にスピードを変える
     float speed = float(i + 1);
     // 画面中央を中心として回転する点の座標
-    vec2 q = p + vec2(cos(t * speed), sin(t * speed)) * 0.6;
+    vec2 q = p + vec2(cos(u_time * speed), sin(u_time * speed)) * 0.7;
     // 描画位置の色を，点qと原点の距離に応じて変化
     destColor += vec3(red, blue, green) / 25. / length(q);
   }
